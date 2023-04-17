733cd3386b6d236123724b86528e2c82f2100a6e
xin jin <lucas1004jx@gmail.com>
2023-04-17 13:59:54 +0200

    it-19326 add us flag icon

--- src/components/Icon/iconComponents.ts
+++ src/components/Icon/iconComponents.ts
@@ -1,19 +1,21 @@
 import { FC, SVGProps } from 'react';
 import { ReactComponent as LogoRebrand } from 'src/assets/icons/logo-rebrand.svg';
-import { ReactComponent as Arrow } from 'src/assets/icons/Arrow.svg';
-import { ReactComponent as Close } from 'src/assets/icons/Close.svg';
+import { ReactComponent as IconArrow } from 'src/assets/icons/arrow.svg';
+import { ReactComponent as IconClose } from 'src/assets/icons/close.svg';
 import { ReactComponent as Promo } from 'src/assets/icons/pill-status.svg';
 import { ReactComponent as IconContact } from 'src/assets/icons/contact.svg';
 import { ReactComponent as IconUser } from 'src/assets/icons/user.svg';
 import { ReactComponent as IconSearch } from 'src/assets/icons/search.svg';
+import { ReactComponent as FlagUs } from 'src/assets/icons/flag-us.svg';
 import { IconName } from 'src/types/IconNames';
 
 export const iconComponents: Record<IconName, FC<SVGProps<SVGSVGElement>>> = {
   'logo-rebrand': LogoRebrand,
-  arrow: Arrow,
-  close: Close,
+  arrow: IconArrow,
+  close: IconClose,
   'pill-status': Promo,
   contact: IconContact,
   user: IconUser,
   search: IconSearch,
+  'flag-us': FlagUs,
 };
