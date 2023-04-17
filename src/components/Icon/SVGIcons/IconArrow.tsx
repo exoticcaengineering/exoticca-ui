310a34e2effeee4328d75ddbf4b7a75917b199cd
xin jin <lucas1004jx@gmail.com>
2023-04-17 16:22:58 +0200

    it-19326 change how to import icons

---
+++ src/components/Icon/SVGIcons/IconArrow.tsx
@@ -0,0 +1,11 @@
+export const IconArrow = () => (
+  <svg viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M9 5L5 1L1 5"
+      stroke="#121212"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
--- src/components/Icon/iconComponents.ts
+++ src/components/Icon/iconComponents.ts
@@ -1,13 +1,14 @@
 import { FC, SVGProps } from 'react';
-import { ReactComponent as LogoRebrand } from 'src/assets/icons/logo-rebrand.svg';
-import { ReactComponent as IconArrow } from 'src/assets/icons/arrow.svg';
-import { ReactComponent as IconClose } from 'src/assets/icons/close.svg';
-import { ReactComponent as Promo } from 'src/assets/icons/pill-status.svg';
-import { ReactComponent as IconContact } from 'src/assets/icons/contact.svg';
-import { ReactComponent as IconUser } from 'src/assets/icons/user.svg';
-import { ReactComponent as IconSearch } from 'src/assets/icons/search.svg';
-import { ReactComponent as FlagUs } from 'src/assets/icons/flag-us.svg';
+
 import { IconName } from 'src/types/IconNames';
+import { IconArrow } from './SVGIcons/IconArrow';
+import { IconClose } from './SVGIcons/IconClose';
+import { Promo } from './SVGIcons/Promo';
+import { IconContact } from './SVGIcons/IconContact';
+import { LogoRebrand } from './SVGIcons/LogoRebrand';
+import { IconUser } from './SVGIcons/IconUser';
+import { IconSearch } from './SVGIcons/IconSearch';
+import { FlagUs } from './SVGIcons/FlagUs';
 
 export const iconComponents: Record<IconName, FC<SVGProps<SVGSVGElement>>> = {
   'logo-rebrand': LogoRebrand,
