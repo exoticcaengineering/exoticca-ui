20ac96a24321d36acdc9aa1d79f3ed1fb9f86346
Mario Sanz <mariosanz@MacBook-Pro-de-Mario-Sanz.local>
2023-04-24 10:37:45 +0200

    added accordion

---
+++ src/components/Accordion/Accordion.styles.tsx
@@ -0,0 +1,54 @@
+import styled, { css } from 'styled-components';
+
+export const Header = styled.h3<{
+  isOpen?: boolean;
+  titleBackground?: string;
+  isDisabled?: boolean;
+  customAccordionButton?: boolean;
+}>`
+  display: flex;
+  justify-content: space-between;
+  padding: 0;
+  border-radius: ${({ theme }) =>
+    `${theme.newBorderRadius.xs} ${theme.newBorderRadius.xs} 0 0 `};
+  margin-top: 0;
+  margin-bottom: 0;
+
+  ${(props) =>
+    props.isOpen &&
+    !props.titleBackground &&
+    css`
+      padding-bottom: 0;
+    `}
+
+  ${(props) =>
+    props.isDisabled &&
+    css`
+      opacity: 0.3;
+      cursor: not-allowed;
+      padding-bottom: 24px;
+    `}
+`;
+
+export const AccordionButton = styled.div<{
+  isOpen: boolean;
+  isDisabled: boolean;
+}>`
+  transition: 0.3ms ease-out;
+  ${(props) =>
+    props.isOpen &&
+    !props.isDisabled &&
+    css`
+      transform: rotate(180deg);
+    `};
+`;
+
+export const AccordionContent = styled.div<{ isOpen: boolean }>`
+  display: flex;
+  align-items: flex-end;
+  flex-wrap: wrap;
+  word-break: break-word;
+  opacity: ${(props) => (props.isOpen ? 1 : 0)};
+  transform: translateY(${(props) => (props.isOpen ? '15px' : 0)});
+  transition: opacity 0.3s ease-in-out, transform 0.2s ease-in-out;
+`;
---
+++ src/components/Accordion/__stories__/Accordion.stories.tsx
@@ -0,0 +1,57 @@
+import { ComponentMeta, Story } from '@storybook/react';
+import { ComponentProps } from 'react';
+import { Accordion } from '../Accordion';
+import { Typography } from 'src/components/Typography';
+import { iconNames } from 'src/types/IconNames';
+
+export default {
+  title: 'Components/Accordion',
+  component: Accordion,
+  args: {
+    isDisabled: false,
+    withHeaderTitle: true,
+    headerText: 'I am a Header Text :)',
+    isOpen: false,
+    icon: 'arrow',
+    contentText:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat risus eu varius fringilla. Fusce luctus ipsum vitae nunc tincidunt, id tincidunt mauris accumsan.',
+  },
+  argTypes: {
+    icon: {
+      control: { type: 'select' },
+      options: [...iconNames, ''],
+    },
+  },
+} as ComponentMeta<typeof Accordion>;
+
+type CustomItemProps = ComponentProps<typeof Accordion> & {
+  withHeaderTitle: boolean;
+  headerText: string;
+  contentText: string;
+};
+
+const Template: Story<CustomItemProps> = ({
+  withHeaderTitle,
+  headerText,
+  isOpen,
+  contentText,
+  isDisabled,
+  icon,
+}: CustomItemProps) => {
+  return (
+    <div style={{ height: '10rem', width: '15rem' }}>
+      <Accordion
+        icon={icon}
+        isOpen={isOpen}
+        isDisabled={isDisabled}
+        header={
+          withHeaderTitle ? (
+            <Typography fontSize="body1">{headerText}</Typography>
+          ) : undefined
+        }
+        content={<Typography>{contentText}</Typography>}
+      />
+    </div>
+  );
+};
+export const Base = Template.bind({});
