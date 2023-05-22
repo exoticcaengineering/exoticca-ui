825809144b850ce6eced2e38a1687f5c0f2dd122
Christian Corona <christian_corona@outlook.com>
2023-05-22 15:58:32 +0200

    IT-17652-add-baggage-icons: Updated icons

--- src/components/Icon/SVGIcons/IconHandBaggage.tsx
+++ src/components/Icon/SVGIcons/IconHandBaggage.tsx
@@ -10,29 +10,29 @@ export const IconHandBaggage = () => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M92.625 97.5H24.375C18.9881 97.5 14.625 93.1369 14.625 87.75V46.3125C14.625 40.9256 18.9881 36.5625 24.375 36.5625H92.625C98.0119 36.5625 102.375 40.9256 102.375 46.3125V87.75C102.375 93.1369 98.0119 97.5 92.625 97.5Z"
-      stroke="#323232"
-      strokeWidth="1.5566"
+      stroke="currentColor"
+      strokeWidth="5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M76.2694 36.5625V28.8941C76.2694 23.5072 71.9063 19.1441 66.5194 19.1441H50.4758C45.0889 19.1441 40.7258 23.5072 40.7258 28.8941V36.5625"
-      stroke="#323232"
-      strokeWidth="1.3416"
+      stroke="currentColor"
+      strokeWidth="5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M36.5625 36.5625V97.5"
-      stroke="#323232"
-      strokeWidth="1.5"
+      stroke="currentColor"
+      strokeWidth="5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M80.4375 63.375V36.5625"
-      stroke="#323232"
-      strokeWidth="1.5"
+      stroke="currentColor"
+      strokeWidth="5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
@@ -40,15 +40,15 @@ export const IconHandBaggage = () => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M63.375 63.375H87.75V78H63.375V63.375Z"
-      stroke="#323232"
-      strokeWidth="1.5"
+      stroke="currentColor"
+      strokeWidth="5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M80.4375 97.5V78"
-      stroke="#323232"
-      strokeWidth="1.5"
+      stroke="currentColor"
+      strokeWidth="5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
