2396de44437eb9c321c6c9602e77d2c5bd727082
xin jin <lucas1004jx@gmail.com>
2023-08-03 17:04:44 +0200

    highlight selectable box

--- src/components/SelectableBox/SelectableBox.styles.ts
+++ src/components/SelectableBox/SelectableBox.styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 import { Box } from '../Box';
+import { StyledProps } from './SelectableBox.types';
+
+export const StyledWrapper = styled.div`
+  position: relative;
+`;
 
 export const StyledBox = styled(Box)`
   overflow: hidden;
@@ -17,4 +22,22 @@ export const SelectedIconWrapper = styled.div`
   top: 0;
   left: 0;
   transform: rotate(45deg) translate(-70%, 0%);
+`;
+
+export const StyledRecommendedTag = styled.div<StyledProps>`
+  position: absolute;
+  width: 50%;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -100%);
+  background: ${({
+    theme,
+    highlightColor = 'promotion',
+    highlightColorShade = 'main',
+  }) => theme.palette[highlightColor][highlightColorShade]};
+  padding: ${({ theme }) => theme.spacing(0.5, 3)};
+  border-radius: ${({ theme }) => theme.newBorderRadius.m}
+    ${({ theme }) => theme.newBorderRadius.m} 0 0;
+  color: ${({ theme, highlightColor = 'promotion' }) =>
+    theme.palette[highlightColor].contrast};
 `;
--- src/components/SelectableBox/SelectableBox.tsx
+++ src/components/SelectableBox/SelectableBox.tsx
@@ -1,32 +1,64 @@
 import { FC } from 'react';
 import { Props } from './SelectableBox.types';
-import { SelectedIconWrapper, StyledBox } from './SelectableBox.styles';
+import {
+  SelectedIconWrapper,
+  StyledBox,
+  StyledRecommendedTag,
+  StyledWrapper,
+} from './SelectableBox.styles';
 import { Icon } from '../Icon';
+import { TextBody2 } from '../TypographyVariants';
 
 export const SelectableBox: FC<Props> = ({
   isSelected,
   children,
+  highlightColor = 'promotion',
+  highlightColorShade = 'main',
+  highlightText,
   ...boxProps
 }) => {
+  const hasHighlight = !!highlightText;
+  const getBorderColor = () => {
+    if (isSelected) return 'primary';
+    if (hasHighlight) return highlightColor;
+    return 'primary';
+  };
+
+  const getBorderShade = () => {
+    if (isSelected) return 'main';
+    if (hasHighlight) return highlightColorShade;
+    return 'light';
+  };
+
   return (
-    <StyledBox
-      borderColor="primary"
-      borderColorShade={isSelected ? 'main' : 'light'}
-      borderWidth={1}
-      padding={[2]}
-      {...boxProps}
-    >
-      {children}
-      {isSelected && (
-        <SelectedIconWrapper>
-          <Icon
-            icon="check"
-            stroke="background"
-            strokeShade="main"
-            rotate={-45}
-          />
-        </SelectedIconWrapper>
+    <StyledWrapper>
+      <StyledBox
+        borderColor={getBorderColor()}
+        borderColorShade={getBorderShade()}
+        borderWidth={1}
+        padding={[2]}
+        {...boxProps}
+      >
+        {children}
+        {isSelected && (
+          <SelectedIconWrapper>
+            <Icon
+              icon="check"
+              stroke="background"
+              strokeShade="main"
+              rotate={-45}
+            />
+          </SelectedIconWrapper>
+        )}
+      </StyledBox>
+      {hasHighlight && (
+        <StyledRecommendedTag
+          highlightColor={highlightColor}
+          highlightColorShade={highlightColorShade}
+        >
+          <TextBody2 align="center">{highlightText}</TextBody2>
+        </StyledRecommendedTag>
       )}
-    </StyledBox>
+    </StyledWrapper>
   );
 };
