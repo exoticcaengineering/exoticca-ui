2743f554190443c59bcf1d82f9e56e78669985d9
xin jin <lucas1004jx@gmail.com>
2023-10-20 11:00:07 +0200

    add svg icons

--- src/components/SvgIcon/SvgIcon.tsx
+++ src/components/SvgIcon/SvgIcon.tsx
@@ -1,30 +1,45 @@
-import { useEffect, useRef, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import IconFallback from '../../assets/svg/fallback.svg';
+import { StyledWrapper } from './SvgIcon.styles';
+import { Props } from './SvgIcon.types';
 
-export const SvgIcon = ({
-  icon,
-}: {
-  icon: 'arrow' | 'flagMX' | 'microfono';
-}) => {
+export const SvgIcon: FC<Props> = ({ icon, testId, ...props }) => {
   const iconRef = useRef<any>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const importIcon = async () => {
-    setIsLoading(true);
-    const importedIcon = await import(`../../assets/${icon}.svg`);
-    iconRef.current = importedIcon.default;
-    setIsLoading(false);
-  };
+
+  const defaultTestId = `${icon}-icon`;
+  const isClickable = !!props.onClick;
 
   useEffect(() => {
+    const importIcon = async () => {
+      setIsLoading(true);
+      const importedIcon = await import(`../../assets/svg/${icon}.svg`);
+      iconRef.current = importedIcon.default;
+      setIsLoading(false);
+    };
     importIcon();
   }, []);
 
-  if (isLoading || !iconRef.current) return <div>loading...</div>;
+  if (isLoading || !iconRef.current)
+    return (
+      <StyledWrapper
+        data-testid={testId || defaultTestId}
+        isClickable={isClickable}
+        {...props}
+      >
+        <IconFallback />
+      </StyledWrapper>
+    );
 
   const Icon = iconRef.current;
 
   return (
-    <div>
+    <StyledWrapper
+      data-testid={testId || defaultTestId}
+      isClickable={isClickable}
+      {...props}
+    >
       <Icon />
-    </div>
+    </StyledWrapper>
   );
 };
