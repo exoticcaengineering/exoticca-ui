0231e608f5d81829b32896bf4311a46faceb6890
xin jin <lucas1004jx@gmail.com>
2023-10-30 10:41:31 +0100

    change Icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,20 +1,44 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { StyledWrapper } from './Icon.styles';
 import { Props } from './Icon.types';
 
-import { StyledWrapper } from './Icon.styles';
-import { iconComponents } from './iconComponents';
-
 export const Icon: FC<Props> = ({ icon, testId, ...props }) => {
+  const iconRef = useRef<any>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const defaultTestId = `${icon}-icon`;
-  const IconComponent = iconComponents[icon] ?? iconComponents['fallback'];
   const isClickable = !!props.onClick;
+
+  useEffect(() => {
+    const importIcon = async () => {
+      setIsLoading(true);
+      const importedIcon = await import(`../../assets/svg/${icon}.svg`);
+      iconRef.current = importedIcon.default;
+      setIsLoading(false);
+    };
+    importIcon();
+  }, []);
+
+  if (isLoading || !iconRef.current)
+    return (
+      <StyledWrapper
+        data-testid={testId || defaultTestId}
+        isClickable={isClickable}
+        {...props}
+      >
+        asdsa
+      </StyledWrapper>
+    );
+
+  const Icon = iconRef.current;
+
   return (
     <StyledWrapper
       data-testid={testId || defaultTestId}
       isClickable={isClickable}
       {...props}
     >
-      <IconComponent />
+      <Icon />
     </StyledWrapper>
   );
 };
