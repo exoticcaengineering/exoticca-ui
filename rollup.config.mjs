2e1c484adc929342112e2953b589cb44e8269034
xin jin <lucas1004jx@gmail.com>
2023-12-01 12:58:31 +0100

    test multi input

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,6 +8,7 @@ import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import filesize from 'rollup-plugin-filesize';
 import svgr from '@svgr/rollup';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
 
 const plugins = [
   peerDepsExternal(),
@@ -58,18 +59,82 @@ const plugins = [
 //   },
 // ];
 
-export default {
-  input: {
-    index: 'src/index.ts',
-    'components/index': 'src/components/index.ts',
-    'themes/index': 'src/themes/index.ts',
+export default [
+  {
+    input: {
+      index: 'src/index.ts',
+      'components/index': 'src/components/index.ts',
+      'themes/index': 'src/themes/index.ts',
+    },
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: true,
+      interop: 'esModule',
+      exports: 'named',
+    },
+    plugins,
   },
-  output: {
-    dir: 'dist',
-    format: 'esm',
-    sourcemap: true,
-    interop: 'esModule',
-    exports: 'named',
+  {
+    input: 'dist/components/index.d.ts',
+    output: [{ file: 'dist/components/index.d.ts', format: 'esm' }],
+    plugins: [
+      dts({
+        compilerOptions: {
+          baseUrl: '.',
+        },
+      }),
+      generatePackageJson({
+        baseContents: {
+          name: '@exoticcaengineering/exoticca-ui/components',
+          private: true,
+          main: '../index.js', // --> points to cjs format entry point of whole library
+          module: './index.js', // --> points to esm format entry point of individual component
+          types: './index.d.ts', // --> points to types definition file of individual component
+        },
+      }),
+    ],
   },
-  plugins,
-};
+  {
+    input: 'dist/themes/index.d.ts',
+    output: [{ file: 'dist/themes/index.d.ts', format: 'esm' }],
+    plugins: [
+      dts({
+        compilerOptions: {
+          baseUrl: '.',
+        },
+      }),
+      generatePackageJson({
+        baseContents: {
+          name: '@exoticcaengineering/exoticca-ui/themes',
+          private: true,
+          main: '../index.js', // --> points to cjs format entry point of whole library
+          module: './index.js', // --> points to esm format entry point of individual component
+          types: './index.d.ts', // --> points to types definition file of individual component
+        },
+      }),
+    ],
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/types/index.d.ts', format: 'esm' }],
+    plugins: [
+      dts({
+        compilerOptions: {
+          baseUrl: '.',
+        },
+      }),
+    ],
+  },
+  {
+    input: 'dist/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [
+      dts({
+        compilerOptions: {
+          baseUrl: '.',
+        },
+      }),
+    ],
+  },
+];
