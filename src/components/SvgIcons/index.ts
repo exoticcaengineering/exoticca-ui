2b33f0cf1c9ce6a3be356eb3107ef4569beb0b90
xin jin <lucas1004jx@gmail.com>
2023-12-28 12:59:05 +0100

    add affirm logo

--- src/components/SvgIcons/index.ts
+++ src/components/SvgIcons/index.ts
@@ -1,5 +1,6 @@
 export { default as AdventureIcon } from './Adventure';
 export { default as AffiliateStarIcon } from './AffiliateStar';
+export { default as AffirmIcon } from './Affirm';
 export { default as AlertTriangleIcon } from './AlertTriangle';
 export { default as AppStoreDeIcon } from './AppStoreDe';
 export { default as AppStoreEnIcon } from './AppStoreEn';
