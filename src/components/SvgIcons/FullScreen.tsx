a1aeac6f195467936e906d5010a568085734b1e1
Christian Corona <christian_corona@outlook.com>
2024-02-29 18:02:17 +0100

    build missing

---
+++ src/components/SvgIcons/FullScreen.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+import { WithIconWrapper } from 'src/components/WithIconWrapper';
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+export const FullScreenIcon = WithIconWrapper(
+  ({
+    title = 'full-screen-icon',
+    titleId,
+    ...props
+  }: SVGProps<SVGSVGElement> & SVGRProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="none"
+      viewBox="0 0 33 32"
+      data-testid={title}
+      aria-labelledby={titleId}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M23.696 8.458 8.61 23.543M16.152 8.457h7.544v7.544M16.156 23.542H8.612v-7.545"
+      />
+    </svg>
+  ),
+);
+export default FullScreenIcon;
